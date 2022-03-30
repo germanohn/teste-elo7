c2377e1b9fc7597e72a6d5b40d7bb5f70bc6f551
Germano <germanohn8@gmail.com>
2022-03-30 14:56:39 -0300

    return Sonda after saving and rename save Planalto

--- src-java/sondas/src/main/java/com/elo7/sondas/domain/SondaService.java
+++ src-java/sondas/src/main/java/com/elo7/sondas/domain/SondaService.java
@@ -12,17 +12,19 @@ public class SondaService {
         return sondas;
     }
 
-    public void save(int max_x, int max_y) {
+    public void setPlanalto(int max_x, int max_y) {
         Planalto.max_x = max_x;
         Planalto.max_y = max_y;
     }
 
-    public void save(Sonda sonda, @NotNull String instructions) {
+    public Sonda save(Sonda sonda, @NotNull String instructions) {
         System.out.println(instructions);
         for (char instruction : instructions.toCharArray()) {
             sonda.move(instruction);
         }
         sonda.print_position();
         sondas.add(sonda);
+
+        return sonda;
     }
 }
