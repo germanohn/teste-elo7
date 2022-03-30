82e65c0ec620dc5d0ee4d8d1e1f7643015721e0e
Germano <germanohn8@gmail.com>
2022-03-30 08:34:22 -0300

    Add basic solution in Java
    This solution receives the instructions through the console

---
+++ src-java/sondas/src/main/java/com/elo7/sondas/domain/Main.java
@@ -0,0 +1,34 @@
+package com.elo7.sondas.domain;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int max_x;
+    public static int max_y;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        max_x = scanner.nextInt();
+        max_y = scanner.nextInt();
+
+        // System.out.println("max_x " + max_x + " max_y " + max_y);
+
+        while (scanner.hasNext()) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            char direction = scanner.next().charAt(0);
+            Sonda sonda = new Sonda(x, y, direction);
+
+            scanner.nextLine();
+            String instructions = scanner.nextLine();
+            /* System.out.println("x " + x + " y " + y);
+            System.out.println("direction " + direction);
+            System.out.println("instructions " + instructions)*/
+            for (char instruction : instructions.toCharArray()) {
+                sonda.move(instruction);
+            }
+
+            sonda.print_position();
+        }
+    }
+}
---
+++ src-java/sondas/src/main/java/com/elo7/sondas/domain/Sonda.java
@@ -0,0 +1,86 @@
+package com.elo7.sondas.domain;
+
+import java.util.TreeMap;
+
+public class Sonda {
+    private int x, y;
+    private int direction;
+
+    public static final TreeMap<Character, Integer> number_of_direction = new TreeMap<>();
+    public static final TreeMap<Integer, Character> symbol_of_direction = new TreeMap<>();
+    public static final TreeMap<Character, Integer> rotation = new TreeMap<>();
+    public static final int[] dx = {0, 1, 0, -1}; // N, E, S, W
+    public static final int[] dy = {1, 0, -1, 0}; // N, E, S, W
+
+    static {
+        number_of_direction.put('N', 0);
+        number_of_direction.put('E', 1);
+        number_of_direction.put('S', 2);
+        number_of_direction.put('W', 3);
+
+        symbol_of_direction.put(0, 'N');
+        symbol_of_direction.put(1, 'E');
+        symbol_of_direction.put(2, 'S');
+        symbol_of_direction.put(3, 'W');
+
+        rotation.put('L', -1);
+        rotation.put('R', +1);
+    }
+
+    // constructors
+    public Sonda() {}
+
+    public Sonda(int x, int y, char direction) {
+        this.x = x;
+        this.y = y;
+        this.direction = number_of_direction.get(direction);
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getDirection() {
+        return direction;
+    }
+
+    // Receive a instruction ('L': rotate 90° left, 'R': rotate 90° right, or 'M': move)
+    // and change the "sonda" position accordingly
+    public void move(char instruction) {
+        if (instruction == 'L' || instruction == 'R') {
+            final int MOD = 4;
+            direction = (direction + rotation.get(instruction) + MOD) % MOD;
+        } else { // instruction == 'M'
+            if (sonda_valid_move(this)) {
+                x += dx[direction];
+                y += dy[direction];
+            }
+        }
+    }
+
+    public void print_position() {
+        /*int x = sonda.getX();
+        int y = sonda.getY();
+        int direction = sonda.getDirection();*/
+        System.out.println(x + " " + y + " " + symbol_of_direction.get(direction));
+    }
+
+    // auxiliary functions
+    private boolean valid_x(int x) {
+        return x >= 0 && x <= Main.max_x;
+    }
+
+    private boolean valid_y(int y) {
+        return y >= 0 && y <= Main.max_y;
+    }
+
+    private boolean sonda_valid_move(Sonda sonda) {
+        return valid_x(sonda.getX() + dx[sonda.getDirection()]) && valid_y(sonda.getY() +
+                dy[sonda.getDirection()]);
+    }
+}
